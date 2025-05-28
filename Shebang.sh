c917ee324f8a73fe2488a5a185e61c29136810a5
lakshmansandeep006 <lakshmansandeep006@gmail.com>
2025-05-28 23:21:56 +0530

    learnbash

--- Shebang.sh
+++ Shebang.sh
@@ -5,4 +5,10 @@ echo welcome to Bash scripting
 echo line1
 echo line2
 
+
+# ('-e'   enables the Escape Sequence characters)
+# ('\n' - Next line)
 echo -e "line3 \n line4"
+
+# (\t   - Tab spaces)
+echo -e "line5 \t line6"
