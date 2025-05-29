322d9c9e3315057b20c8c43acf263540d4de880b
lakshmansandeep006 <lakshmansandeep006@gmail.com>
2025-05-29 17:10:35 +0530

    1

--- variables.sh
+++ variables.sh
@@ -15,7 +15,7 @@ echo "value of a is $a"
 echo "value of b is $b"
 
 # Now lets try printing it with colors
-echo "Now lets try printing it with colors"
+echo "---Now lets try printing it with colors---"
 
 # example
 echo -e "value of a is \e[32m $a \e[0m"
@@ -25,3 +25,25 @@ echo -e "value of b is \e[32m $b \e[0m"
 # Ex: in java we have int a=10 , char a=x, string a=mike.
 
 # * In Bash everything is will be considerd as String by default. 
+
+
+# * when we are using special variable , single quate is  going to  nullify the speciality of the special variable
+
+# example :
+echo $a           #(this is corrcet syntax)
+echo ${a}         #(this is also corrcet syntax)
+echo "$a"         #(this is also corrcet syntax)
+
+echo '$a'         #(This single quate nullify the specality of special variable)
+
+# *singe quate should not be used for special variable
+
+
+#__________________________________________________________________________________________________
+### if try to print a variable that is not declared,what will happen? Bash considers that as null value
+
+#For example:- if we run a below script, with out Declaring the CUSTDATA value.
+# rm -rf /data/prod/${CUSDATA}
+
+# The script considers the prod directory as a Target directory and delets everything..so it is very important to declare the value correctly. 
+
