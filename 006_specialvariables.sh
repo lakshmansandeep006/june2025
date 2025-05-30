6912aa6d455d914e5e58f3ab79394523ac9bec03
lakshmansandeep006 <lakshmansandeep006@gmail.com>
2025-05-30 12:35:27 +0530

    1

--- 006_specialvariables.sh
+++ 006_specialvariables.sh
@@ -11,9 +11,10 @@
 # echo $*  # prints all arguments used as a single string
 # echo $@  # prints all arguments used as seperate words
 
-echo $0  # Prints the Script name being run
-echo $1  # prints the first argument provided in CLI after the script file
-echo "Number of Arguments used are $#"  # prinst the number of argumnets used in the script
-echo $?  # prints the Exit status ( also called the return code) of the last command that was run
-echo " Arguments used are $* "  # prints all arguments used as a single string
-echo " Arguments used are printed seperetly $@"  # prints all arguments used as seperate words
+# Example :-
+echo "The Script Name is $0"                    # Prints the Script name being run
+echo "script executed by $1"                    # prints the first argument provided in CLI after the script file
+echo "Number of Arguments used are $#"          # prinst the number of argumnets used in the script
+echo "The exit code of last execution is $?"    # prints the Exit status ( also called the return code) of the last command that was run
+echo " Arguments used are $* "                  # prints all arguments used as a single string
+echo " Arguments used are printed seperetly $@" # prints all arguments used as seperate words
